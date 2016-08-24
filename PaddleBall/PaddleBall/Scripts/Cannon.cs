feb87b63b6d3a78d9cd44d5afba979b56458124e
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2016-08-24 01:12:03 -0600

    First commit

---
+++ PaddleBall/PaddleBall/Scripts/Cannon.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaddleBall {
+    public class Cannon {
+
+        bool isClockWise = true;
+        Texture2D image;
+        ContentManager content;
+
+        public void LoadContent() {
+            string path = "Images/Cannon";
+            image = content.Load<Texture2D>(path);
+        }
+
+        public void SwitchDirection() {
+            isClockWise = !isClockWise;
+        }
+
+        public void Update() {
+            //rotate clockwise by default
+        }
+    }
+}
