cfdcbccf62c2e58f27b8b56dbabefcd1313e004b
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2016-09-01 09:41:49 -0600

    Shield now visually changes on collision

--- PaddleBall/PaddleBall/Scripts/GameObjects/Shield.cs
+++ PaddleBall/PaddleBall/Scripts/GameObjects/Shield.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using System.Diagnostics;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PaddleBall {
     public class Shield : GameObject {
 
         CircleCollider myCollider;
+        List<Texture2D> textures = new List<Texture2D>();
         int health = 3;
 
         private static Shield instance;
@@ -25,12 +27,19 @@ namespace PaddleBall {
 
         float scaleSize = 0.5f;
         public override void LoadContent(ContentManager Content) {
-            texturePath = "Images/Barrier";
+            
             SetLayerDepth(0f);
             scale = Vector2.One * scaleSize;
             myCollider = new CircleCollider(Layer.Shield, this, 350 * scaleSize);
             position = screenCenter;
-            base.LoadContent(Content);
+            content = Content;
+            textures.Add(content.Load<Texture2D>("Images/RED_BARRIER"));
+            textures.Add(content.Load<Texture2D>("Images/ORANGE_BARRIER"));
+            textures.Add(content.Load<Texture2D>("Images/Barrier"));
+        }
+
+        public override void PostLoad() {
+            SetOriginInPixels(textures[health-1].Width / 2, textures[health - 1].Height / 2);
         }
 
         public override void Update(GameTime gameTime) {
@@ -51,6 +60,10 @@ namespace PaddleBall {
             if (health == 0) {
                 Destroy();
             }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(textures[health-1], position, null, color, rotation, originInPixels, scale, flip, layerDepth);
         }
 
     }
